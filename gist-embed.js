87b17e63a2d4a0b0dde97ae3a6daa2041b90054a
Kashif Umair Liaqat <kashif.umair@7vals.com>
2013-01-31 16:15:14 +0500

    fix #5

--- gist-embed.js
+++ gist-embed.js
@@ -55,6 +55,7 @@ $(function(){
               head.insertBefore(l, head.firstChild);
             }
             if(line){
+              console.log(typeof(response.div));
               lineCode = response.div.innerHTML.filter('#file-' + splittedFileName + '-LC' + line);
               $elem.html(response.div);
             }
