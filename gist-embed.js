bea7f4894838dc0e570adc0efe6d62f04108bfb6
Kashif Umair Liaqat <kashif.umair@7vals.com>
2013-01-31 17:54:53 +0500

    Update gist-embed.js

--- gist-embed.js
+++ gist-embed.js
@@ -55,12 +55,13 @@ $(function(){
               head.insertBefore(l, head.firstChild);
             }
             if(line){
-              lineCode = $('<div>').append($(response.div).filter('#file-' + splittedFileName + '-LC' + line)).html(); //'#file-' + splittedFileName + '-LC' + line);
+              lineCode = $(response.div).find('#file-' + splittedFileName + '-LC' + line)[0].html();
               basicStructureWithSingleLine = '<div id="gist' + id + '" class="gist"><div class="gist-file">' +
                                              '<div class="gist-data gist-syntax"><div class="file-data">' +
                                              '<table cellpadding="0" cellspacing="0" class="lines highlight">' +
-                                             '<tbody><tr><td class="line-data"><pre class="line-pre">' + lineCode + '</pre>' +
-                                             '</td></tr></tbody></table></div></div></div></div>';
+                                             '<tbody><tr><td class="line-data"><pre class="line-pre">' +
+                                             '<div class="line" id="file-' + splittedFileName + '-LC' + line + '">' +
+                                             lineCode + '</div></pre></td></tr></tbody></table></div></div></div></div>';
               $elem.html(basicStructureWithSingleLine);
             }
             else{
